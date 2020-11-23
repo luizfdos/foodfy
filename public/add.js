abce73478e73b699ab442469ce97b1adf4d81380
LuizFDOS <luizfernando.os@outlook.com>
2020-11-23 17:10:03 -0300

    funcao para campos dinamicos ingrediente e preparo

---
+++ public/add.js
@@ -0,0 +1,17 @@
+function addItem(divId, divClass) {
+  const ingredients = document.querySelector(divId);
+  const fieldContainer = document.querySelectorAll(divClass);
+  // Realiza um clone do último ingrediente adicionado
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+
+  // Não adiciona um novo input se o último tem um valor vazio
+  if (newField.children[0].value == "") return false;
+
+  // Deixa o valor do input vazio
+  newField.children[0].value = "";
+  ingredients.appendChild(newField);
+}
+
+// document
+//   .querySelector(".add-ingredient")
+//   .addEventListener("click", addItem("#ingredients", ".ingredient"));
